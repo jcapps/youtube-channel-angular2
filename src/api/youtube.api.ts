28b61fd39330de2d85ce25a60d5d6dfa0433b789
jcapps <jamesmcapps@gmail.com>
2017-04-17 09:41:43 -0500

    Remove unused import

--- src/api/youtube.api.ts
+++ src/api/youtube.api.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, URLSearchParams, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import gapiHelper from './gapi-helper';
 
